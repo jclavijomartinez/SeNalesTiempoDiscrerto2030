f6fc3a733961a1f663d557d729c45f68059a8401
Juan Clavijo <jclavijomartinez@gmail.com>
2020-10-03 18:02:04 -0500

    complemento proyecto2, estaba incompleto

--- proyecto2.m
+++ proyecto2.m
@@ -87,8 +87,87 @@ end
 %% 2. Inversion en el tiempo
 [palindromaJ fs1]=audioread('palindromaJ.wav');
 [palindromaS fs1]=audioread('palindromaS.wav');
-[palindromaF fs1]=audioread('palindromaf.wav');
-
-%% 3. Desplazamiento en el tempo 
+[palindromaF fs1]=audioread('palindromaF.wav');
+inversionpalindromaJ=flipud(palindromaJ);
+inversionpalindromaS=flipud(palindromaS);
+inversionpalindromaF=flipud(palindromaF);
+[escalatiempoFSJ fs1]=audioread('escalatiempoFSJ.wav');
+disp('Estas son las palindromas, elija una porfavor')
+disp('1. frase antes del sistema de Juan Clavijo')
+disp('2. frase después del sistema de Juan Clavijo')
+disp('3. frase antes del sistema de Santiago Mesa')
+disp('4. frase después del sistema de Santiago Mesa')
+disp('5. frase antes del sistema de Fabio Sánchez')
+disp('6. frase después del sistema de Fabio Sánchez')
+userinputp = input('opcion: ');
+switch userinputp
+    case 1
+        audiowrite('fraseantesJuan.wav',palindromaJ,fs1);
+        disp('Presione cualquier tecla, para escuchar el audio')
+        pause
+        soundsc(palindromaJ,fs1)
+    case 2
+        audiowrite('frasedespuesJuan.wav',inversionpalindromaJ,fs1);
+        disp('Presione cualquier tecla, para escuchar el audio invertido')
+        [inversionpalindromaJ fs1]=audioread('frasedespuesJuan.wav');
+        pause
+        soundsc(inversionpalindromaJ,fs1)
+    case 3
+        audiowrite('fraseantesSantiago.wav',palindromaS,fs1);
+        disp('Presione cualquier tecla, para escuchar el audio')
+        pause
+        soundsc(palindromaS,fs1)
+    case 4
+        audiowrite('frasedespuesSantiago.wav',inversionpalindromaS,fs1);
+        disp('Presione cualquier tecla, para escuchar el audio invertido')
+        [inversionpalindromaS fs1]=audioread('frasedespuesSantiago.wav');
+        pause
+        soundsc(inversionpalindromaS,fs1)
+    case 5
+        audiowrite('fraseantesFabio.wav',palindromaF,fs1);
+        disp('Presione cualquier tecla, para escuchar el audio')
+        pause
+        soundsc(palindromaF,fs1)
+    case 6
+        audiowrite('frasedespuesFabio.wav',inversionpalindromaF,fs1);
+        disp('Presione cualquier tecla, para escuchar el audio invertido')
+        [inversionpalindromaF fs1]=audioread('frasedespuesFabio.wav');
+        pause
+        soundsc(inversionpalindromaF,fs1)
+end
+%% 3. Desplazamiento en el tiempo 
+[holaacaestoyJSF fs1]=audioread('holaacaestoyJSF.wav');
+audioleido=holaacaestoyJSF(:,:)
+N=length(audioleido);
+%posicion 1
+RetardoL=zeros(N+30,2);
+RetardoL(1:N,1)=audioleido(:,1);
+RetardoL(31:N+30,2)=audioleido(:,1);
+soundsc(RetardoL,fs1)
+audiowrite('posicion1.wav',RetardoL,fs1);
+%posicion 2
+RetardoLF=zeros(N+15,2);
+RetardoLF(1:N,1)=audioleido(:,1);
+RetardoLF(16:N+15,2)=audioleido(:,1);
+pause
+soundsc(RetardoLF,fs1)
+audiowrite('posicion2.wav',RetardoLF,fs1);
+%posicion 3
+pause
+soundsc(holaacaestoyJSF,fs1)
+%posicion 4
+RetardoRF=zeros(N+15,2);
+RetardoRF(1:N,2)=audioleido(:,1);
+RetardoRF(16:N+15,1)=audioleido(:,1);
+pause
+soundsc(RetardoRF,fs1)
+audiowrite('posicion4.wav',RetardoRF,fs1);
+%posicion 5
+RetardoR=zeros(N+30,2);
+RetardoR(1:N,2)=audioleido(:,1);
+RetardoR(31:N+30,1)=audioleido(:,1);
+pause
+soundsc(RetardoR,fs1)
+audiowrite('posicion5.wav',RetardoR,fs1);
 %% 4. Respuesta impulso de un slit
 %% 5. Obtener la respuesta impulso de un SLIT
