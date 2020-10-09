ece665eca9ad4ed87cbe16c16992477f9db767cd
Juan Clavijo <jclavijomartinez@gmail.com>
2020-10-09 17:01:41 -0500

    orden .gitignore y codigo

--- Proyectos/proyecto2.m
+++ Proyectos/proyecto2.m
@@ -26,80 +26,85 @@ while 1
     opcion = input('¿Cual punto desea ejecutar?: ')
     while 1
         switch opcion
-        case 1
-            %% 1. Escalamiento en el tiempo
-            disp(' ');
-            [escalatiempoFSJ fs1]=audioread('Audios/escalatiempoFSJ.wav');
-            disp('Estas son las velocidades disponibles, elija una porfavor');
-            disp('1. 0.5x');
-            disp('2. 0.65x');
-            disp('3. 0.8x');
-            disp('4. 1x');
-            disp('5. 1.20x');
-            disp('6. 1.35x');
-            disp('7. 1.5x');
-            disp('8. 2x');
-            userinput = input('opcion: ');
-            switch userinput
-                case 1
-                    audiowrite('0.5xescalatiempoFSJ.wav',escalatiempoFSJ,0.5*fs1);
-                    disp('Presione cualquier tecla, para escuchar el audiox0.5')
-                    [CeroCincoEscalatiempoFSJ fs1]=audioread('0.5xescalatiempoFSJ.wav');
-                    pause
-                    soundsc(CeroCincoEscalatiempoFSJ,fs1)
-                    break;
-                case 2
-                    audiowrite('0.65xescalatiempoFSJ.wav',escalatiempoFSJ,0.65*fs1);
-                    disp('Presione cualquier tecla, para escuchar el audiox0.65')
-                    [CeroSesentayCincoEscalatiempoFSJ fs1]=audioread('0.65xescalatiempoFSJ.wav');
-                    pause
-                    soundsc(CeroSesentayCincoEscalatiempoFSJ,fs1)
-                    break;
-                case 3
-                    audiowrite('0.8xescalatiempoFSJ.wav',escalatiempoFSJ,0.80*fs1);
-                    disp('Presione cualquier tecla, para escuchar el audiox0.80')
-                    [CeroOchoEscalatiempoFSJ fs1]=audioread('0.8xescalatiempoFSJ.wav');
-                    pause
-                    soundsc(CeroOchoEscalatiempoFSJ,fs1)
-                    break;
-                case 4
-                    audiowrite('1xescalatiempoFSJ.wav',escalatiempoFSJ,fs1);
-                    disp('Presione cualquier tecla, para escuchar el audiox1.00')
-                    [UnoEscalatiempoFSJ fs1]=audioread('1xescalatiempoFSJ.wav');
-                    pause
-                    soundsc(UnoEscalatiempoFSJ,fs1)
-                    break;
-                case 5
-                    audiowrite('1.2xescalatiempoFSJ.wav',escalatiempoFSJ,1.20*fs1);
-                    disp('Presione cualquier tecla, para escuchar el audiox1.20')
-                    [UnoVeinteEscalatiempoFSJ fs1]=audioread('1.2xescalatiempoFSJ.wav');
-                    pause
-                    soundsc(UnoVeinteEscalatiempoFSJ,fs1)
-                    break;
-                case 6
-                    audiowrite('1.35xescalatiempoFSJ.wav',escalatiempoFSJ,1.35*fs1);
-                    disp('Presione cualquier tecla, para escuchar el audiox1.35')
-                    [UnoTreintayCincoEscalatiempoFSJ fs1]=audioread('1.35xescalatiempoFSJ.wav');
-                    pause
-                    soundsc(UnoTreintayCincoEscalatiempoFSJ,fs1)
-                    break;
-                case 7
-                    audiowrite('1.5xescalatiempoFSJ.wav',escalatiempoFSJ,1.50*fs1);
-                    disp('Presione cualquier tecla, para escuchar el audiox1.50')
-                    [UnoCincuentaEscalatiempoFSJ fs1]=audioread('Audios/1.5xescalatiempoFSJ.wav');
-                    pause
-                    soundsc(UnoCincuentaEscalatiempoFSJ,fs1)
-                    break;
-                case 8
-                    audiowrite('2xescalatiempoFSJ.wav',escalatiempoFSJ,2*fs1);
-                    disp('Presione cualquier tecla, para escuchar el audiox2')
-                    [DosEescalatiempoFSJ fs1]=audioread('2xescalatiempoFSJ.wav');
-                    pause
-                    soundsc(DosEescalatiempoFSJ,fs1)
-                    break;
-            end
+        %% 1. Escalamiento en el tiempo
+            case 1
+                while 1
+                    disp(' ');
+                    [escalatiempoFSJ fs1]=audioread('Audios/escalatiempoFSJ.wav');
+                    disp('Estas son las velocidades disponibles, elija una porfavor');
+                    disp('1. 0.5x');
+                    disp('2. 0.65x');
+                    disp('3. 0.8x');
+                    disp('4. 1x');
+                    disp('5. 1.20x');
+                    disp('6. 1.35x');
+                    disp('7. 1.5x');
+                    disp('8. 2x');
+                    disp('9. regresar al menu principal');
+                    userinput = input('opcion: ');
+                    switch userinput
+                        case 1
+                            audiowrite('0.5xescalatiempoFSJ.wav',escalatiempoFSJ,0.5*fs1);
+                            disp('Presione cualquier tecla, para escuchar el audiox0.5')
+                            [CeroCincoEscalatiempoFSJ fs1]=audioread('0.5xescalatiempoFSJ.wav');
+                            pause
+                            soundsc(CeroCincoEscalatiempoFSJ,fs1)
+                            break;
+                        case 2
+                            audiowrite('0.65xescalatiempoFSJ.wav',escalatiempoFSJ,0.65*fs1);
+                            disp('Presione cualquier tecla, para escuchar el audiox0.65')
+                            [CeroSesentayCincoEscalatiempoFSJ fs1]=audioread('0.65xescalatiempoFSJ.wav');
+                            pause
+                            soundsc(CeroSesentayCincoEscalatiempoFSJ,fs1)
+                            break;
+                        case 3
+                            audiowrite('0.8xescalatiempoFSJ.wav',escalatiempoFSJ,0.80*fs1);
+                            disp('Presione cualquier tecla, para escuchar el audiox0.80')
+                            [CeroOchoEscalatiempoFSJ fs1]=audioread('0.8xescalatiempoFSJ.wav');
+                            pause
+                            soundsc(CeroOchoEscalatiempoFSJ,fs1)
+                            break;
+                        case 4
+                            audiowrite('1xescalatiempoFSJ.wav',escalatiempoFSJ,fs1);
+                            disp('Presione cualquier tecla, para escuchar el audiox1.00')
+                            [UnoEscalatiempoFSJ fs1]=audioread('1xescalatiempoFSJ.wav');
+                            pause
+                            soundsc(UnoEscalatiempoFSJ,fs1)
+                            break;
+                        case 5
+                            audiowrite('1.2xescalatiempoFSJ.wav',escalatiempoFSJ,1.20*fs1);
+                            disp('Presione cualquier tecla, para escuchar el audiox1.20')
+                            [UnoVeinteEscalatiempoFSJ fs1]=audioread('1.2xescalatiempoFSJ.wav');
+                            pause
+                            soundsc(UnoVeinteEscalatiempoFSJ,fs1)
+                            break;
+                        case 6
+                            audiowrite('1.35xescalatiempoFSJ.wav',escalatiempoFSJ,1.35*fs1);
+                            disp('Presione cualquier tecla, para escuchar el audiox1.35')
+                            [UnoTreintayCincoEscalatiempoFSJ fs1]=audioread('1.35xescalatiempoFSJ.wav');
+                            pause
+                            soundsc(UnoTreintayCincoEscalatiempoFSJ,fs1)
+                            break;
+                        case 7
+                            audiowrite('1.5xescalatiempoFSJ.wav',escalatiempoFSJ,1.50*fs1);
+                            disp('Presione cualquier tecla, para escuchar el audiox1.50')
+                            [UnoCincuentaEscalatiempoFSJ fs1]=audioread('Audios/1.5xescalatiempoFSJ.wav');
+                            pause
+                            soundsc(UnoCincuentaEscalatiempoFSJ,fs1)
+                            break;
+                        case 8
+                            audiowrite('2xescalatiempoFSJ.wav',escalatiempoFSJ,2*fs1);
+                            disp('Presione cualquier tecla, para escuchar el audiox2')
+                            [DosEescalatiempoFSJ fs1]=audioread('2xescalatiempoFSJ.wav');
+                            pause
+                            soundsc(DosEescalatiempoFSJ,fs1)
+                            break;
+                        case 9
+                            break;
+                    end
+                end
+%% 2. Inversion en el tiempo
             case 2
-                %% 2. Inversion en el tiempo
                 disp(' ');
                 [palindromaJ fs1]=audioread('Audios/palindromaJ.wav');
                 [palindromaS fs1]=audioread('Audios/palindromaS.wav');
