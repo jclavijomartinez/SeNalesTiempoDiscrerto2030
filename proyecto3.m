ba375887c77ec7521dfa1c871d173c489448f22f
Juan Clavijo <jclavijomartinez@gmail.com>
2020-10-16 09:32:36 -0500

    actividad 0

--- proyecto3.m
+++ proyecto3.m
@@ -1,1 +1,7 @@
-[]
+[uno fs1]=audioread('AudiosP3/1.wav');
+[dos fs2]=audioread('AudiosP3/2.wav');
+[tres fs3]=audioread('AudiosP3/3.wav');
+[cuatro fs4]=audioread('AudiosP3/4.wav');
+[barrido fs5]=audioread('AudiosP3/Barrido_incremental.wav');
+[tonocuatrocuarenta fs6]=audioread('AudiosP3/tono_440.wav');
+[tonopuro fs7]=audioread('AudiosP3/Tono_puro01.wav');
